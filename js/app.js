57913500ac1fda04bcea47bba8a1989902036425
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-10-27 00:21:48 -0700

    Adds five questions to prompts

--- js/app.js
+++ js/app.js
@@ -9,21 +9,22 @@ while (!userName) {
   }
   if (userName) {
     alert(`Thanks for dropping in, ${userName}! I heard you wanted to get to know me a little... I'll give you some info about me and then we'll play a guessing game!`);
-
   } else alert("Sorry, I didn't have my hearing aids in..."); 
-
 };
 
-document.getElementById("start").addEventListener("click", ()=>{
-  let answer1 = prompt(`?`);
+function guessingGame() {
+  let answer1 = prompt(`DO I HAVE ANY PETS?
+  (Y/N)`);
   alert(``);
-  let answer2 = prompt(``);
+  let answer2 = prompt(`DO I LIVE ON THE EAST COAST? (Y/N)`);
   alert(``);
-  let answer3 = prompt(``);
+  let answer3 = prompt(`AM I CLEVER? (Y/N)`);
   alert(``);
-  let answer4 = prompt(``);
+  let answer4 = prompt(`DO I WORK ON CARS FOR FUN? (Y/N)`);
   alert(``);
-  let answer5 = prompt(`One more question. ...?`);
+  let answer5 = prompt(`Last question! AM I AN ALGORITHM? (Y/N)`);
   alert(``);
-});
+}
+document.getElementById("start").addEventListener("click", guessingGame());
+
 // INCOMPLETE, need to add answer evaluation and responses, CSS styling for all elements, etc.
